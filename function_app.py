32c68c5a34f10adf3de51aa2a91acbbda059ab3c
chrisean nichols <chriseannichols550@gmail.com>
2024-01-13 11:44:50 -0500

    init

--- function_app.py
+++ function_app.py
@@ -17,10 +17,16 @@ def ArticleSummary(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(json.loads(f'{"text":"Hello, {name}. This HTTP triggered function executed successfully."}'))
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(
+            body = json.loads('{"text":"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."}'),
+            status_code=200,
+            mimetype="application/json"
+    )
+    
+    # if name:
+    #     return func.HttpResponse(json.loads(f'{"text":"Hello, {name}. This HTTP triggered function executed successfully."}'))
+    # else:
+    #     return func.HttpResponse(
+    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    #          status_code=200
+    #     )
