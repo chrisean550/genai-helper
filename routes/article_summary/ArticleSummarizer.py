080f3617bb045cefa1c8c512c2f4ecf7ba02ac3f
chrisean nichols <chriseannichols550@gmail.com>
2024-01-22 20:59:05 -0500

    Should be able to handle conversations now

--- routes/article_summary/ArticleSummarizer.py
+++ routes/article_summary/ArticleSummarizer.py
@@ -10,15 +10,22 @@ import time
 
 # Authenticate session with Open AI CLIENT
 CLIENT = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-REQUEST_LIMIT = 10
+REQUEST_LIMIT = 15
 SUCCESS = ["completed"]
 INCOMPLETE = ["queued", "in_progress", None]
 FAILED = ["cancelled", "failed", "expired"]
 
 def ArticleSummarizer(req: func.HttpRequest):
     # Getting info from the request body
-    body = req.get_json()
-
+    try:
+        body = req.get_json()
+    except:
+        return func.HttpResponse(
+            body = json.dumps({"message": "Dude you didn't have any json in here"}),
+            status_code = 401,
+            mimetype = "application/json"
+        )
+    
     # End the thread if param is true
     if body.get("end_thread") and validThread(body.get("thread_id")):
         return deleteThread(body.get("thread_id"))
@@ -26,33 +33,34 @@ def ArticleSummarizer(req: func.HttpRequest):
     # If there is content, check if it is a part of an existing conversation
     if body.get("content"):
         if validThread(body.get("thread_id")):
-            logging.warning("good thread")
-            return func.HttpResponse(
-            body = json.dumps({"message": "Support for conversations not yet implemented"}),
-            status_code = 400,
-            mimetype = "application/json"
-        )
+            logging.info("Existing thread, continuing conversation.")
+            return appendConversation(body.get("content"), body.get("thread_id"))
         else:
             logging.info("Not an existing thread. Starting new conversation.")
-            return startConversation(body.get("content"))
+            return appendConversation(body.get("content"), None)
     else:
         logging.warning("no content")
         return func.HttpResponse(
-            body = json.dumps({"message": "No content provided"}),
+            body = json.dumps({"message": "Sorry but it seems like you didn't provide any instructions"}),
             status_code = 400,
             mimetype = "application/json"
         )
     
+
 # WARNING THIS DELETES THE THREAD SO DONT BE STUPID AND NOT KNOW WHY CONVERSATION ISNT WORKING    
 # Main function for starting a conversations and returning initial summary
-def startConversation(content) -> func.HttpResponse:
-    run_res = getRunResult(initiateThread(content))
+def appendConversation(content, thread_id) -> func.HttpResponse:
+    if thread_id:
+        run_res = getRunResult(continueThread(content, thread_id))
+    else:
+        run_res = getRunResult(initiateThread(content))
 
     if run_res.status in INCOMPLETE:
         logging.warning("Run timed out.")
         return func.HttpResponse(
             body = json.dumps(
                 {"run_id": run_res.id,
+                 "thread_id": run_res.thread_id,
                  "result": run_res,
                  "message": "Run timed out"}),
             status_code = 408,
@@ -64,6 +72,7 @@ def startConversation(content) -> func.HttpResponse:
         return func.HttpResponse(
             body = json.dumps(
                 {"run_id": run_res.id,
+                 "thread_id": run_res.thread_id,
                  "result": run_res,
                  "message": "Run failed"}),
             status_code = 409,
@@ -73,12 +82,11 @@ def startConversation(content) -> func.HttpResponse:
     
     logging.info("Run succeeded, getting message.")
     result = getMessage(run_res.thread_id, run_res.id)
-    
-    deleteThread(run_res.thread_id)
 
     return func.HttpResponse(
             body = json.dumps(
                 {"run_id": run_res.id,
+                 "thread_id": run_res.thread_id,
                  "message": result}),
             status_code = 200,
             mimetype = "application/json"
@@ -154,6 +162,21 @@ def initiateThread(content):
 
     return res
 
+# Starts a new thread with content, returns run object
+def continueThread(content, thread_id):
+    message = CLIENT.beta.threads.messages.create(
+        thread_id = thread_id,
+        role = "user",
+        content = content
+    )
+
+    res = CLIENT.beta.threads.runs.create(
+        thread_id = thread_id,
+        assistant_id = "asst_FVSL4rY0HlkaV8PBjfuE2Xin"
+    )
+
+    return res
+
 def getRunResult(run_res):
     status = None
     notComplete = ["queued", "in_progress", None]
@@ -161,7 +184,7 @@ def getRunResult(run_res):
     run = None
 
     while status in notComplete and attempts < REQUEST_LIMIT:
-        time.sleep(1)
+        time.sleep(2)
         run = CLIENT.beta.threads.runs.retrieve(
             thread_id = run_res.thread_id,
             run_id = run_res.id
